b40f668938f0c0d2bbf8a9aaa5eecd1c7764cfe0
BRIAN MUENZENMEYER <brian.muenzenmeyer@gmail.com>
2016-09-08 23:11:55 -0500

    WIP configuration writing to public

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 "use strict";
-console.log('index!');
+
+var fs = require('fs-extra'),
+  path = require('path');
+
 function plugin_init(patternlab) {
-  console.log('plugin_init!');
+
+  //write the plugin json to public/patternlab-components
+  var pluginConfig = {
+    "name":"pattern-lab\/plugin-node-tab",
+    "templates":[],
+    "stylesheets":[],
+    "javascripts":["\/js\/plugin-tab.js"],
+    "onready":"PluginTab.init()",
+    "callback":""
+  }
+
+  var pluginConfigPathName = path.resolve(patternlab.config.paths.public.root, 'patternlab-components', 'packages');
+  fs.outputFileSync(pluginConfigPathName + '/plugin-tab.json', JSON.stringify(pluginConfig, null, 2));
 }
 
 module.exports = plugin_init;
